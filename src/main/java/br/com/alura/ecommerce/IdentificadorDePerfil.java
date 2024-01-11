982fdba9f58d49ebe3d0bcdcea45286edd35e374
KievMaia <kiev@prologapp.com>
2024-01-11 13:31:42 -0300

    Implementando o identificador de perfil e importando os arquivos csv.

---
+++ src/main/java/br/com/alura/ecommerce/IdentificadorDePerfil.java
@@ -0,0 +1,43 @@
+package br.com.alura.ecommerce;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.Arrays;
+
+public class IdentificadorDePerfil {
+    public static void main(String[] args) {
+        final var system = """
+                Identifique o perfil de compra de cada cliente.
+                                
+                A resposta deve ser:
+                Cliente - descreva o perfil do cliente em três palavras
+                """;
+
+        final String clientes = carregarClientesDoArquivo();
+        final var chave = System.getenv("OPENAI_API_KEY");
+        final var service = new OpenAiService(chave, Duration.ofSeconds(30));
+
+        final var request = ChatCompletionRequest.builder()
+                .model("gtp-3.5-turbo")
+                .messages(Arrays.asList(new ChatMessage(ChatMessageRole.SYSTEM.value(), system),
+                                        new ChatMessage(ChatMessageRole.USER.value(), clientes)))
+                .build();
+
+        System.out.println(service.createChatCompletion(request).getChoices().get(0).getMessage().getContent());
+    }
+
+    private static String carregarClientesDoArquivo() {
+        try {
+            final var path = Path.of(ClassLoader.getSystemResource("lista_de_compas_10_clientes.csv").toURI());
+            return Files.readAllLines(path).toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao carregar arquivo", e);
+        }
+    }
+}
