f9944c283dc7a8249674d2d739b925ee36bfcdd2
KievMaia <kiev@prologapp.com>
2024-01-11 17:43:31 -0300

    Adicionando o contador de tokens para escolha dinâmica do modelo.

--- src/main/java/br/com/alura/ecommerce/IdentificadorDePerfil.java
+++ src/main/java/br/com/alura/ecommerce/IdentificadorDePerfil.java
@@ -1,5 +1,7 @@
 package br.com.alura.ecommerce;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
@@ -16,16 +18,26 @@ public class IdentificadorDePerfil {
                 Identifique o perfil de compra de cada cliente.
                                 
                 A resposta deve ser:
-                
+                                
                 Cliente - descreva o perfil do cliente em três palavras
                 """;
 
         final String clientes = carregarClientesDoArquivo();
         final var chave = System.getenv("OPENAI_API_KEY");
-        final var service = new OpenAiService(chave, Duration.ofSeconds(30));
+        final var service = new OpenAiService(chave, Duration.ofSeconds(60));
+
+        var quantidadeTokens = contarTokens(clientes);
+
+        var modelo = "gpt-3.5-turbo";
+        var tamanhoRespostaEsperada = 2048;
+        if (quantidadeTokens > 4096 - tamanhoRespostaEsperada) {
+            modelo = "gpt-3.5-turbo-16k";
+        }
+        System.out.println("QTD tokens " + quantidadeTokens);
+        System.out.println("Modelo escolhido " + modelo);
 
         final var request = ChatCompletionRequest.builder()
-                .model("gpt-3.5-turbo")
+                .model(modelo)
                 .messages(Arrays.asList(new ChatMessage(ChatMessageRole.SYSTEM.value(), system),
                                         new ChatMessage(ChatMessageRole.USER.value(), clientes)))
                 .build();
@@ -33,9 +45,15 @@ public class IdentificadorDePerfil {
         System.out.println(service.createChatCompletion(request).getChoices().get(0).getMessage().getContent());
     }
 
+    private static int contarTokens(final String prompt) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_3_5_TURBO);
+        return enc.countTokens(prompt);
+    }
+
     private static String carregarClientesDoArquivo() {
         try {
-            final var path = Path.of(ClassLoader.getSystemResource("lista_de_compras_10_clientes.csv").toURI());
+            final var path = Path.of(ClassLoader.getSystemResource("lista_de_compras_100_clientes.csv").toURI());
             return Files.readAllLines(path).toString();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao carregar arquivo", e);
